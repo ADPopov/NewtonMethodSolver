fe86596d7f0f71695cb591ca45a6178f23ea9443
FeeLeD <feeled9@gmail.com>
2019-10-06 10:44:51 +0500

    v1

---
+++ NewtonMethodSolver/Equation.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewtonMethodSolver
+{
+    class Equation
+    {
+        private double epsilon;
+        private double x0;
+
+        public Equation(double x0, double epsilon)
+        {
+            this.x0 = x0;
+            this.epsilon = epsilon;
+        }
+
+        public double GetSolution()
+        {
+            double x1 = x0 - F(x0) / DerivativeF(x0);
+            while (Math.Abs(x1 - x0) > epsilon)
+            {
+                x0 = x1;
+                x1 = x1 - F(x1) / DerivativeF(x1);
+            }
+            return Round(x1);
+        }
+
+        private double F(double x)
+        {
+            return x * x * x + 0.4 * x + 2;
+        }
+
+        private double DerivativeF(double x)
+        {
+            return 3 * x * x + 0.4;
+        }
+
+        private double Round(double number)
+        {
+            var dotIndex = epsilon.ToString().IndexOf(',');
+            var numbersAfterDot = epsilon.ToString().Substring(dotIndex).Length - 1;
+            return Math.Round(number, numbersAfterDot);
+        }
+    }
+}
--- NewtonMethodSolver/Program.cs
+++ NewtonMethodSolver/Program.cs
@@ -10,6 +10,20 @@ namespace NewtonMethodSolver
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Решение уравнения x^3 + 0.4x + 2 методом Ньютона\n");
+
+            while (true)
+            {
+                double epsilon, x0;
+                Console.Write("Задайте точность вычисления: ");
+                while (!double.TryParse(Console.ReadLine(), out epsilon)) ;
+
+                Console.Write("Введите начальное приближение x0: ");
+                while (!double.TryParse(Console.ReadLine(), out x0)) ;
+
+                var equation = new Equation(x0, epsilon);
+                Console.WriteLine("Ответ: {0}", equation.GetSolution());
+            }
         }
     }
 }
